b658676e06520739cbf56a70471a121098c90f8b
Vladislavs <vladislavs.lavrenovs@gmail.com>
2022-06-22 05:40:19 +0300

    private route

--- src/components/router/AppRouter.component.jsx
+++ src/components/router/AppRouter.component.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect,
+	BrowserRouter as Router,
+	Route,
+	Switch,
+	Redirect,
 } from "react-router-dom";
 import Home from "../../views/Home/Home.view";
 import Login from "../../views/Login/Login.view";
@@ -24,82 +24,76 @@ import Register from "../../views/Register/Register.component";
 import Profile from "../../views/Profile/Profile.view";
 import SearchPage from "../../views/SearchPage/SearchPage.component";
 import ForgotPassword from "../../views/ForgotPassword/ForgotPassword.view";
+import { PrivateRoute } from "./PrivateRoute.component";
 
 export const AppRouter = () => {
-  const { state } = useUserContext();
-  const [header, showHeader] = useState(true);
-  useEffect(() => {
-    showHeader(true);
-  }, []);
-  return (
-    <Router>
-      {header && <Header />}
-      <Switch>
-        <Route path={["/", "/home"]} exact component={() => <Home />} />
-        <Route path={["/login"]} exact component={() => <Login />}>
-          {state.user && <Redirect to="/"></Redirect>}
-        </Route>
-        <Route path={["/register"]} exact component={() => <Register />}>
-          {state.user && <Redirect to="/"></Redirect>}
-        </Route>
-        <Route path={["/add-news"]} exact>
-          {state.user.admin ? <AddNews /> : <NotFound />}
-        </Route>
-        <Route path={["/add-categories"]} exact>
-          {state.user.admin ? <AddCategories /> : <NotFound />}
-        </Route>
-        <Route path={["/list-an-item"]} exact>
-          {state.user ? <ListItem /> : <NotFound />}
-        </Route>
-        <Route path={["/requests"]} exact>
-          {state.user ? <BookingRequests /> : <NotFound />}
-        </Route>
-        <Route path={["/item/:id"]} exact>
-          <ItemPage />
-        </Route>
-        <Route path={["/search"]} exact>
-          <SearchPage />
-        </Route>
-        <Route
-          path={["/forgot-password"]}
-          exact
-          component={() => <ForgotPassword></ForgotPassword>}
-        >
-          {state.user && <Redirect to="/"></Redirect>}
-        </Route>
-        <Route path={["/bookings"]} exact>
-          {state.user ? <MyBookings /> : <NotFound />}
-        </Route>
-        <Route path={["/qr-code/:type?/:booking?"]}>
-          {state.user ? (
-            <QRPage showHeader={showHeader} />
-          ) : (
-            <NotFound></NotFound>
-          )}
-        </Route>
-        <Route path={["/qr-reader/:booking?"]} exact>
-          {state.user ? (
-            <QRRead showHeader={showHeader} />
-          ) : (
-            <NotFound></NotFound>
-          )}
-        </Route>
+	const { state } = useUserContext();
+	const [header, showHeader] = useState(true);
+	useEffect(() => {
+		showHeader(true);
+	}, []);
+	return (
+		<Router>
+			{header && <Header />}
+			<Switch>
+				<Route path={["/", "/home"]} exact component={() => <Home />} />
+				<Route path={["/login"]} exact component={() => <Login />}>
+					{state.user && <Redirect to="/"></Redirect>}
+				</Route>
+				<Route path={["/register"]} exact component={() => <Register />}>
+					{state.user && <Redirect to="/"></Redirect>}
+				</Route>
+				{/* TODO check admin routes */}
+				<PrivateRoute path={["/add-news"]} exact admin={true}>
+					<AddNews />
+				</PrivateRoute>
+				<Route path={["/add-categories"]} exact>
+					{state.user.admin ? <AddCategories /> : <NotFound />}
+				</Route>
+				<Route path={["/list-an-item"]} exact>
+					{state.user ? <ListItem /> : <NotFound />}
+				</Route>
+				<Route path={["/requests"]} exact>
+					{state.user ? <BookingRequests /> : <NotFound />}
+				</Route>
+				<Route path={["/item/:id"]} exact>
+					<ItemPage />
+				</Route>
+				<Route path={["/search"]} exact>
+					<SearchPage />
+				</Route>
+				<Route
+					path={["/forgot-password"]}
+					exact
+					component={() => <ForgotPassword></ForgotPassword>}
+				>
+					{state.user && <Redirect to="/"></Redirect>}
+				</Route>
+				<PrivateRoute path={["/bookings"]} exact>
+					<MyBookings />
+				</PrivateRoute>
+				<PrivateRoute path={["/qr-code/:type?/:booking?"]}>
+					<QRPage showHeader={showHeader} />
+				</PrivateRoute>
+				<PrivateRoute path={["/qr-reader/:booking?"]} exact>
+					<QRRead showHeader={showHeader} />
+				</PrivateRoute>
+				<PrivateRoute path={["/profile"]} exact>
+					<Profile />
+				</PrivateRoute>
 
-        <Route path={["/profile"]} exact>
-          {state.user ? <Profile /> : <NotFound />}
-        </Route>
-        <Route path={["/checkout"]} exact>
-          <CheckoutPage />
-        </Route>
+				<Route path={["/checkout"]} exact>
+					<CheckoutPage />
+				</Route>
 
-        <Route
-          path={["/test-upload"]}
-          exact
-          component={() => <UploadTest />}
-        ></Route>
-        <Route component={() => <NotFound />} />
-      </Switch>
-      {/* <Footer /> */}
-    </Router>
-  );
+				<Route
+					path={["/test-upload"]}
+					exact
+					component={() => <UploadTest />}
+				></Route>
+				<Route component={() => <NotFound />} />
+			</Switch>
+			{/* <Footer /> */}
+		</Router>
+	);
 };
--- src/components/router/PrivateRoute.component.jsx
+++ src/components/router/PrivateRoute.component.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import { Redirect, Route } from "react-router-dom";
 import { useUserContext } from "../../context/user";
-export const PrivateRoute = ({ children, ...props }) => {
-  const { state } = useUserContext();
-  return (
-    <Route
-      {...props}
-      render={({ location }) => {
-        return state.user ? (
-          children
-        ) : (
-          <Redirect to={{ pathname: "/login", state: { from: location } }} />
-        );
-      }}
-    ></Route>
-  );
+import NotFound from "../../views/NotFound";
+
+export const PrivateRoute = ({ children, admin = false, ...props }) => {
+	const { state } = useUserContext();
+	return (
+		<Route
+			{...props}
+			render={({ location }) => {
+				return state.user ? (
+					admin ? (
+						state.user.admin ? (
+							children
+						) : (
+							<NotFound />
+						)
+					) : (
+						children
+					)
+				) : (
+					<Redirect to={{ pathname: "/login", state: { from: location } }} />
+				);
+			}}
+		></Route>
+	);
 };
